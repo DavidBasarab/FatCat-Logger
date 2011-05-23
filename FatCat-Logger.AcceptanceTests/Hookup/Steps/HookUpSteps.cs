b94f0d2bc7fadceb8acf59e34a5a8bc7b91a0e59
David Basarab <dbasarab@virtualhold.com>
2011-05-23 13:10:37 -0400

    Added NBehave acceptance tests to project.  Along with HookUp project.

---
+++ FatCat-Logger.AcceptanceTests/Hookup/Steps/HookUpSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NBehave.Narrator.Framework;
+using NBehave.Spec.NUnit;
+
+namespace FatCat_Logger.AcceptanceTests.Hookup.Steps
+{
+    [ActionSteps]
+    public class HookUpSteps
+    {
+        private int _hookUpNumber;
+
+        [Given]
+        public void Given_I_want_to_test_my_hook_up()
+        {
+            _hookUpNumber = 42;
+        }
+
+        [When]
+        public void When_hook_up_my_tests()
+        {
+            _hookUpNumber = 42 + 10;
+        }
+
+        [Then]
+        public void Then_my_hook_up_should_pass()
+        {
+            _hookUpNumber.ShouldEqual(52);
+        }
+
+    }
+}
